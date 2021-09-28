015855c204d8b1bea2f3964d578d6468c8662dad
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-09-28 15:40:22 +0100

    Borrower has properties

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private String name;
+    private  Integer capacity;
+    private ArrayList<Book> stock;
+
+    public Borrower(String name, Integer capacity){
+        this.name = name;
+        this.capacity = capacity;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int countStock() {
+        return this.stock.size();
+    }
 }
+
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,9 +1,30 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.Buffer;
+
 import static org.junit.Assert.assertEquals;
 
 public class BorrowerTest {
+
+    private Borrower borrower;
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before() {
+        library = new Library(5);
+        book = new Book("1984", "George Orwell", "Dystopian");
+        borrower = new Borrower("Steve", 2);
+
+    }
+
+    @Test
+    public void hasNameCapacityStock(){
+        assertEquals("Steve", borrower.getName());
+        assertEquals(2, borrower.getCapacity());
+        assertEquals(0, borrower.countStock());
+    }
 
 
 }
