ed7955a6efd2a9c051a32f2f34aebcd5d84e22bf
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-09-28 15:14:44 +0100

    MVP Bossed

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private Integer capacity;
+    private ArrayList<Book> stock;
+
+    public Library(Integer capacity){
+        this.capacity = capacity;
+        this.stock = new ArrayList<>();
+    }
+
+    public Object getCapacity() {
+        return this.capacity;
+    }
+
+    public void addBook(Book book) {
+        this.stock.add(book);
+    }
+
+    public int countStock() {
+        return this.stock.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library(5);
+        book = new Book("1984","George Orwell","Dystopian");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(5, library.getCapacity());
+    }
+
+    @Test
+    public void hasBooksInStock(){
+        library.addBook(book);
+        assertEquals(1,library.countStock());
+    }
 }
